bfbd1f3b2f6fe337d567a871b324a643d42b0d9b
23ucs073 <madhubhilave1404@gmail.com>
2025-12-11 10:05:14 +0530

    Add CProg3.java and update Day1.java

---
+++ CProg3.java
@@ -0,0 +1,12 @@
+public class CProg3
+{
+    public static void main(string[]args)
+    {
+        int i=1;
+        while(i<=5)
+        {
+            System.out.println("Welcome");
+            i++;
+        }
+        }
+    }
